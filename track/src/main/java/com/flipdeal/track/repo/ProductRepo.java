13fbb721ae8ae7cf1518184ce929dc53acf2495f
Ravikumar-Singh <77490184+Ravikumar-Singh@users.noreply.github.com>
2021-01-15 22:18:17 +0530

    Update ProductRepo.java

--- track/src/main/java/com/flipdeal/track/repo/ProductRepo.java
+++ track/src/main/java/com/flipdeal/track/repo/ProductRepo.java
@@ -1,6 +1,5 @@
 package com.flipdeal.track.repo;
 
-import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,8 +9,7 @@ import com.flipdeal.track.model.Product;
 @Repository
 public interface ProductRepo extends CrudRepository<Product, String>{
 	
-	Product findByName(String name);
+	Product findByProduct(String product);
 	Product findById(int id);
-	Product findByProductId(String productId);
-	Product findByNameAndType(String name,String type);
+	Product findByProductAndCategory(String product,String category);
 }
